7a8a47161573491db2ffc284289ff6c1d416bcb3
Danula Eranjith <hmdanulae@gmail.com>
2014-12-22 10:16:20 +0530

    Random Forest added

--- src/org/wso2/carbon/ml/extension/RExtension.java
+++ src/org/wso2/carbon/ml/extension/RExtension.java
@@ -3,22 +3,23 @@ package org.wso2.carbon.ml.extension;
 import java.util.List;
 
 import org.rosuda.JRI.REXP;
+import org.rosuda.JRI.RList;
+import org.rosuda.JRI.RVector;
 import org.rosuda.JRI.Rengine;
 import org.wso2.carbon.ml.extension.util.InitializeWorkflow;
 import org.wso2.carbon.ml.model.internal.dto.MLFeature;
 import org.wso2.carbon.ml.model.internal.dto.MLWorkflow;
 
-
 public class RExtension {
-	
+
 	private MLWorkflow mlWorkflow;
-	
-	public RExtension(){
+
+	public RExtension() {
 		this.mlWorkflow = new MLWorkflow();
-	}	
+	}
 
 	public static void main(String[] args) {
-		
+
 		// new R-engine
 		Rengine re = new Rengine(new String[] { "--vanilla" }, false, null);
 		if (!re.waitForR()) {
@@ -26,36 +27,46 @@ public class RExtension {
 			return;
 		}
 
-		
 		RExtension rex = new RExtension();
 		InitializeWorkflow ob = new InitializeWorkflow();
-		rex.mlWorkflow = ob.parseWorkflow("example_workflow.json");		
-		
-		//evaluate MLWorkflow
+		rex.mlWorkflow = ob.parseWorkflow("example_workflow.json");
+
+		// evaluate MLWorkflow
 		evaluate(re, rex.mlWorkflow);
-		
+
 		re.end();
 
 	}
 
 	private static void evaluate(Rengine re, MLWorkflow mlWorkflow) {
-		switch (mlWorkflow.getAlgorithmName()) {
-		case "LOGISTIC_REGRESSION":
-			logisticRegression(re, mlWorkflow);
-		}
 
-	}
-
-	public static void logisticRegression(Rengine re, MLWorkflow mlWorkflow) {
 		// load data from csv
 		re.eval("input <- read.csv('" + mlWorkflow.getDatasetURL() + "')",
 				false);
 
-		List<MLFeature> features = mlWorkflow.getFeatures();
+		System.out.println("input <- read.csv('" + mlWorkflow.getDatasetURL()
+				+ "')");
 
 		StringBuffer script = new StringBuffer();
-		script.append("result <- glm(" + mlWorkflow.getResponseVariable()
-				+ " ~ ");
+		script.append("model <- ");
+		String postfix = null;
+		switch (mlWorkflow.getAlgorithmName()) {
+		case "LOGISTIC_REGRESSION":
+			script.append("glm(");
+			postfix = ", data=input, family=binomial(link='logit'))";
+			break;
+		case "RANDOM_FOREST":
+			re.eval("library(randomForest)", false);
+			script.append("randomForest(");
+			postfix = ",data=input, ntree=50)";
+			break;
+		}
+
+		List<MLFeature> features = mlWorkflow.getFeatures();
+
+		script.append(mlWorkflow.getResponseVariable());
+		script.append(" ~ ");
+
 		boolean flag = false;
 
 		for (int i = 0; i < features.size(); i++) {
@@ -72,26 +83,41 @@ public class RExtension {
 				if (feature.getType().equals("CATEGORICAL")) {
 					String name = feature.getName();
 					re.eval("input$" + name + "<- factor(input$" + name + ")");
+					System.out.println("input$" + name + "<- factor(input$"
+							+ name + ")");
 				}
 
 				// impute
 				if (feature.getImputeOption().equals("REPLACE_WTH_MEAN")) {
 					String name = feature.getName();
 					re.eval("temp <- mean(input$" + name + ",na.rm=TRUE)");
+
+					System.out.println("temp <- mean(input$" + name
+							+ ",na.rm=TRUE)");
+
 					re.eval("input$" + name + "[input$" + name
 							+ "==NA] <- temp");
+					System.out.println("input$" + name + "[input$" + name
+							+ "==NA] <- temp");
+				} else if (feature.getImputeOption().equals("DISCARD")) {
+					re.eval("input[complete.cases(input$" + feature.getName()
+							+ "),]", false);
 				}
 				// removing a row --- newdata <- na.omit(mydata)
 			}
 		}
-		script.append(", data=input, family=binomial(link='logit'))");
-		re.eval(script.toString(), false);
+		script.append(postfix);
+
+		REXP x = re.eval(script.toString());
+
 		System.out.println(script);
 
-		// re.eval("df <- subset(mtcars, select=c(mpg,am,vs))",false);
-		// REXP x = re.eval("coef(result)[['Age']]");
-		REXP x = re.eval("coef(result)");
+		REXP y = re.eval("coef(model)[['Age']]");
+		// RVector x = re.eval("model").asVector();
+
 		System.out.println(x);
+		System.out.println(y);
+
 	}
 
 }
