23af8ed3491e65171d296e0690b0549fc35f796a
Madawa Soysa <madawa.rc@gmail.com>
2015-02-19 19:22:28 +0530

    svm

---
+++ src/main/java/org/wso2/carbon/ml/extension/algorithms/SupportVectorMachine.java
@@ -0,0 +1,27 @@
+package org.wso2.carbon.ml.extension.algorithms;
+
+import org.wso2.carbon.ml.extension.bean.MLRWorkflow;
+import org.wso2.carbon.ml.extension.utils.CommonConstants;
+
+import java.util.ArrayList;
+
+public class SupportVectorMachine extends RAlgorithm {
+
+	public static final String LIB_E1071 = "library('e1071')";
+
+	@Override public ArrayList<String> generatePMML(StringBuilder parameters,
+	                                                MLRWorkflow mlrWorkflow) {
+		ArrayList<String> modelScript = new ArrayList<>();
+		modelScript.add(LIB_E1071);
+		modelScript.add(CommonConstants.LIBRARY_PMML);
+		modelScript
+				.add(CommonConstants.TUNED_MODEL + "<- svm(" + parameters.toString() + ")");
+		modelScript.add(CommonConstants.PMML_MODEL + " <- pmml(" + CommonConstants.TUNED_MODEL + ")");
+
+		return modelScript;
+	}
+
+	@Override public ArrayList<String> generateAdditionalScripts(MLRWorkflow mlRWorkflow) {
+		return null;
+	}
+}
