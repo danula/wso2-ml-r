d850ebe5f311b32b8f7681dee3fa3a9638c37937
Madawa Soysa <madawa.rc@gmail.com>
2015-02-19 19:36:17 +0530

    random forest

---
+++ src/main/java/org/wso2/carbon/ml/extension/algorithms/RandomForest.java
@@ -0,0 +1,28 @@
+package org.wso2.carbon.ml.extension.algorithms;
+
+import org.wso2.carbon.ml.extension.bean.MLRWorkflow;
+import org.wso2.carbon.ml.extension.utils.CommonConstants;
+
+import java.util.ArrayList;
+
+public class RandomForest extends RAlgorithm{
+
+	public static final String LIB_RF = "library('randomForest')";
+
+	@Override public ArrayList<String> generatePMML(StringBuilder parameters,
+	                                                MLRWorkflow mlrWorkflow) {
+
+		ArrayList<String> modelScript = new ArrayList<>();
+		modelScript.add(LIB_RF);
+		modelScript.add(CommonConstants.LIBRARY_PMML);
+		modelScript
+				.add(CommonConstants.TUNED_MODEL + "<- randomForest(" + parameters.toString() + ")");
+		modelScript.add(CommonConstants.PMML_MODEL + " <- pmml(" + CommonConstants.TUNED_MODEL + ")");
+
+		return modelScript;
+	}
+
+	@Override public ArrayList<String> generateAdditionalScripts(MLRWorkflow mlRWorkflow) {
+		return null;
+	}
+}
