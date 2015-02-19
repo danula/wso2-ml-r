d4fd9e4b007f4037645bbb09ab1af9bcf65e169b
Madawa Soysa <madawa.rc@gmail.com>
2015-02-19 19:45:11 +0530

    decision tree

---
+++ src/main/java/org/wso2/carbon/ml/extension/algorithms/DecisionTree.java
@@ -0,0 +1,22 @@
+package org.wso2.carbon.ml.extension.algorithms;
+
+import org.wso2.carbon.ml.extension.bean.MLRWorkflow;
+import org.wso2.carbon.ml.extension.utils.CommonConstants;
+
+import java.util.ArrayList;
+
+public class DecisionTree extends RAlgorithm{
+
+	@Override public ArrayList<String> generatePMML(StringBuilder parameters,
+	                                                MLRWorkflow mlrWorkflow) {
+		ArrayList<String> modelScript = new ArrayList<>();
+		modelScript.add(CommonConstants.LIBRARY_PMML);
+		modelScript.add(CommonConstants.PMML_MODEL + " <- pmml(" + CommonConstants.FINAL_MODEL + ")");
+
+		return modelScript;
+	}
+
+	@Override public ArrayList<String> generateAdditionalScripts(MLRWorkflow mlRWorkflow) {
+		return null;
+	}
+}
